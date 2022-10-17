a73bfd935173550fbc5de6fa6238e4aec0896200
craterellus <98069492+craterellus@users.noreply.github.com>
2022-10-17 00:54:49 -0400

    Add time zone functionality

--- tz.js
+++ tz.js
@@ -1,16 +1,22 @@
 
 const moment = require('moment-timezone');
+const yargs = require('yargs');
 
 moment.tz.setDefault('America/New_York');
 
+const command = yargs.argv._[0];
+console.log(yargs.argv);
+// console.log(process.argv)
 let targetTimezone;
 
-if (process.argv.length != 3) {
+if (command === false) {
     console.log("Usage: node <script-file> <timezone>");
     process.exit(1);
 }
-else {
-    targetTimezone = process.argv[2];
-}
 
-console.log(`The time at the ${targetTimezone} timezone is ${moment().tz(targetTimezone).format()}`);
+if (yargs.argv.format === true) {
+    console.log(`The time at the ${command} timezone is ${moment().tz(command).format('dddd, MMMM Do YYYY, h:mm:ss a')}`);
+}
+else {
+    console.log(`The time at the ${command} timezone is ${moment().tz(command).format()}`);
+}
